20dd8adce37696990d9bd71d2037b5a11010f3d9
Blake Brown <blakebrown20000502@gmail.com>
2017-03-10 12:59:31 -0700

    vsd

---
+++ random beep/random beep/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+void randombeep();
+
+int main() {
+	
+	for (int i = 0;i < 500;i++)
+		randombeep();
+}
+
+
+void randombeep() {
+	int number = rand() % 600;
+	int number2 = rand() % 899;
+	Beep(number, number2);
+}
